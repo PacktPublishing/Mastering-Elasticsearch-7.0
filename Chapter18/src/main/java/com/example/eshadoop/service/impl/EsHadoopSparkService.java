f9b319dac84cccdbd9eb845f7621115f63865bab
wtwong316 <wtwong316@gmail.com>
2019-06-24 00:04:47 -0700

    clean up project

--- Chapter18/src/main/java/com/example/eshadoop/service/impl/EsHadoopSparkService.java
+++
@@ -1,9 +0,0 @@
-package com.example.eshadoop.service.impl;
-
-import java.util.Map;
-
-
-public interface EsHadoopSparkService {
-	Map<String, Object> buildAnomalyDetectionModel(String indexName, String[] fieldName, int numOfClass);
-
-}
