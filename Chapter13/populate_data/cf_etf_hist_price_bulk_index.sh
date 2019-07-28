fa8a68bdf22efeb520a5ad73061de501bf8550d7
wtwong316 <wtwong316@gmail.com>
2019-07-28 17:46:16 -0400

    fix the datatype of symbol to keyword

--- Chapter13/populate_data/cf_etf_hist_price_bulk_index.sh
+++ Chapter13/populate_data/cf_etf_hist_price_bulk_index.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -XDELETE localhost:9200/cf_etf_hist_price
-curl -XPUT localhost:9200/cf_etf_hist_price -H "Content-Type:application/json" --data-binary @cf_etf_hist_price_mappings.json
+curl -XPUT localhost:9200/cf_etf_hist_price -H "Content-Type:application/json" --data-binary @define_custom_analyzer.json
 curl -XPOST localhost:9200/cf_etf_hist_price/_bulk?pretty -H "Content-Type:application/json" --data-binary @cf_etf_hist_price_bulk.json
